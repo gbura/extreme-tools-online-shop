46c5d354ceb0f4b3a4425cff1e4bbcb23802306f
gbura <rage6999@wp.pl>
2024-03-15 00:37:47 +0100

    save

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -4,6 +4,7 @@ import instanceAxios from '@/axios'
 export const useAuthStore = defineStore('auth', {
 	state: () => ({
 		token: localStorage.getItem('BearerToken') || null,
+		userId: localStorage.getItem('userId') || null,
 	}),
 	getters: {
 		isAuthenticated: state => !!state.token,
@@ -12,12 +13,17 @@ export const useAuthStore = defineStore('auth', {
 		async login(email, password) {
 			const response = await instanceAxios.post('authenticate', { email, password })
 			const token = await response.data.data.token
+			const userId = await response.data.data.user.id
 			this.token = token
+			this.userId = userId
 			localStorage.setItem('BearerToken', token)
+			localStorage.setItem('userId', userId)
 		},
 		async logout() {
 			localStorage.removeItem('BearerToken')
+			localStorage.removeItem('userId')
 			this.token = null
+			this.userId = null
 		},
 		async register(email, password, companyName, companyAddress) {
 			const response = await instanceAxios.post('bo/users', {
